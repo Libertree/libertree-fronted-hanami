af8c08b210b9ebc0ddd204029d38eca41e4e5d8d
Pistos <git dawt pistos aet purepistos dawt net>
2018-02-21 03:05:05 -0500

    Set up Libertree::DB correctly on startup

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,14 @@
 require 'bundler/setup'
+
+########################
+# FIXME: Sequel wants us to connect to the db before defining models.  As model
+# definitions are loaded when 'libertree/model' is required, we have to do
+# this first.
+require 'libertree/db'
+Libertree::DB.load_config "#{File.dirname( __FILE__ ) }/database.yaml"
+Libertree::DB.dbh
+########################
+
 require 'hanami/setup'
 require_relative '../apps/web/application'
 require_relative './initializers/locale.rb'
