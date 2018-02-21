5b05ca43708026d9a493e48d0727be158dd9195e
Pistos <git dawt pistos aet purepistos dawt net>
2018-02-21 03:29:21 -0500

    Enumerate posts in template

--- spec/web/views/home/index_spec.rb
+++ spec/web/views/home/index_spec.rb
@@ -1,7 +1,17 @@
 RSpec.describe Web::Views::Home::Index, type: :view do
   let(:account) { FactoryBot.create(:account) }
+  let(:post) {
+    FactoryBot.create(
+      :post,
+      member_id: account.member.id,
+    )
+  }
+
   let(:exposures) {
-    { current_account: account, }
+    {
+      current_account: account,
+      posts: [post],
+    }
   }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/home/index.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
@@ -9,6 +19,13 @@ RSpec.describe Web::Views::Home::Index, type: :view do
 
   it 'has a title' do
     expect(rendered).to match /Home/
+  end
+
+  it "shows the account's username" do
     expect(rendered).to include account.username
+  end
+
+  it 'shows the text of the posts' do
+    expect(rendered).to include post.text
   end
 end
