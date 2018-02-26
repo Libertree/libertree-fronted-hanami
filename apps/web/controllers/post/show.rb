39104bef96384b525ab211d1cccf73b01467fd64
Pistos <git dawt pistos aet purepistos dawt net>
2018-02-26 03:42:55 -0500

    Mark post as read/seen after showing it

--- apps/web/controllers/post/show.rb
+++ apps/web/controllers/post/show.rb
@@ -7,7 +7,12 @@ module Web::Controllers::Post
     def call(params)
       @post = Libertree::Model::Post[params[:id].to_i]
 
-      halt 404  if @post.nil?
+      if @post.nil?
+        halt 404
+      else
+        @post.mark_as_read_by(current_account)
+        Libertree::Model::Notification.mark_seen_for_account_and_post(current_account, @post)
+      end
     end
   end
 end
