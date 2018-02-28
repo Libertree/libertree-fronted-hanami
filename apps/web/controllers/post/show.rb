bf4bb62c58abd4d7fc7cfa2dd8bf4e5da548b1bb
Pistos <git dawt pistos aet purepistos dawt net>
2018-02-28 17:02:21 -0500

    Go to home instead of 404 if post not found

--- apps/web/controllers/post/show.rb
+++ apps/web/controllers/post/show.rb
@@ -8,7 +8,8 @@ module Web::Controllers::Post
       @post = Libertree::Model::Post[params[:id].to_i]
 
       if @post.nil?
-        halt 404
+        flash[:error] = I18n.t('Could not display post.')
+        redirect_to routes.path(:home)
       else
         @post.mark_as_read_by(current_account)
         Libertree::Model::Notification.mark_seen_for_account_and_post(current_account, @post)
--- spec/web/controllers/post/show_spec.rb
+++ spec/web/controllers/post/show_spec.rb
@@ -1,9 +1,28 @@
 RSpec.describe Web::Controllers::Post::Show, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[id: post.id] }
+  let(:params) { {id: post.id} }
+  let(:call) { action.call(params) }
+  let(:flash) { action.exposures[:flash] }
 
   include_context '[signed in]' do
     let(:current_account) { FactoryBot.create(:account) }
+
+    context '[the id is invalid]' do
+      let(:post) { Libertree::Model::NilPost.new }
+
+      it 'sets the error flash' do
+        call
+
+        expect(flash[:error]).to match(/could not display/i)
+      end
+
+      it 'redirects to home' do
+        response = call
+
+        expect(response[0]).to eq 302
+        expect(response[1]['Location']).to eq '/home'
+      end
+    end
 
     context '[a post exists]' do
       let(:post) {
@@ -14,13 +33,13 @@ RSpec.describe Web::Controllers::Post::Show, type: :action do
       }
 
       it 'is successful' do
-        response = action.call(params)
+        response = call
 
         expect(response[0]).to eq 200
       end
 
       it 'exposes the post' do
-        action.call(params)
+        call
 
         expect(action.exposures).to have_key(:post)
         expect(action.exposures[:post]).to eq post
